40e846689a82df79ccf56bec98d7642455420a32
Batuhan Ekinci <38223126+batuhanekinci@users.noreply.github.com>
2018-04-09 14:06:17 -0700

    Update blinky_gui.py

--- blinky_gui.py
+++ blinky_gui.py
@@ -1,19 +1,5 @@
 #!/usr/bin/env python
 
-"""Extract blink data and visualize it using matplotlib.
-
-
-"""
-    
-__author__           = "Dilawar Singh"
-__copyright__        = "Copyright 2015, Dilawar Singh and NCBS Bangalore"
-__credits__          = ["NCBS Bangalore"]
-__license__          = "GNU GPL"
-__version__          = "1.0.0"
-__maintainer__       = "Dilawar Singh"
-__email__            = "dilawars@ncbs.res.in"
-__status__           = "Development"
-
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as anim
