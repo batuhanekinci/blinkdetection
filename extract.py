2ad8d7cb824067a862900562233548da62d9c6cd
Batuhan Ekinci <38223126+batuhanekinci@users.noreply.github.com>
2018-04-09 14:08:05 -0700

    Update extract.py

--- extract.py
+++ extract.py
@@ -1,18 +1,4 @@
-#!/usr/bin/env python
-"""process_blink_csv.py: 
 
-    Process the csv file and extract the blink data.
-
-"""
-    
-__author__           = "Dilawar Singh"
-__copyright__        = "Copyright 2015, Dilawar Singh and NCBS Bangalore"
-__credits__          = ["NCBS Bangalore"]
-__license__          = "GNU GPL"
-__version__          = "1.0.0"
-__maintainer__       = "Dilawar Singh"
-__email__            = "dilawars@ncbs.res.in"
-__status__           = "Development"
 
 import numpy as np
 import pylab
