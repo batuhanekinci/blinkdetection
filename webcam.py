5e4e9ce6a07ad6a6a28f96c69d8c5cce554cab8f
Batuhan Ekinci <38223126+batuhanekinci@users.noreply.github.com>
2018-04-09 14:07:05 -0700

    Update webcam.py

--- webcam.py
+++ webcam.py
@@ -1,16 +1,3 @@
-"""
-Extract a csv file out of video file representing eye blinks.
-
-"""
-    
-__author__           = "Dilawar Singh"
-__copyright__        = "Copyright 2015, Dilawar Singh and NCBS Bangalore"
-__credits__          = ["NCBS Bangalore"]
-__license__          = "GNU GPL"
-__version__          = "1.0.0"
-__maintainer__       = "Dilawar Singh"
-__email__            = "dilawars@ncbs.res.in"
-__status__           = "Development"
 
 import cv2
 import numpy as np
